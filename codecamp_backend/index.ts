52147caa62794eed39a86a8b56814ce3275da4ad
이수민 <isoomin@isumin-ui-MacBookPro.local>
2024-06-28 15:30:44 +0900

    ORM과 DB연동

--- codecamp_backend/index.ts
+++ codecamp_backend/index.ts
@@ -1,3 +1,25 @@
-const qqq:string = "안녕하세요~~";
+// const qqq:string = "안녕하세요~~";
 
-console.log(qqq);
+// console.log(qqq);
+
+import { DataSource } from "typeorm"
+import { Board } from "./Board.postgres"
+
+const AppDataSource = new DataSource({
+    type: "postgres",
+    host: "34.64.244.122",
+    port: 5001,
+    username: "postgres",
+    password: "postgres2022",
+    database: "postgres",
+    entities: [Board],
+    synchronize: true,
+    logging: true,
+})
+
+AppDataSource.initialize().then(() => {
+    console.log("DB 접속에 성공했습니다!!!")
+}).catch((error) => {
+    console.log("DB 접속에 실패했습니다!!!")
+    console.log("원인: ", error)
+});
